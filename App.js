3bf868a000ae8e5f4d0940e221ee69c1328965db
AlexanderKohr <94537064+AlexanderKohr@users.noreply.github.com>
2022-04-12 18:33:23 +0200

    initialized react-native app

--- App.js
+++ App.js
@@ -1,55 +1,36 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, TextInput, Text, Alert, Button, ScrollView } from 'react-native';
+import React from 'react';
 
-export default class HelloWorld extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { text: '' };
-  }
+// import the screens
+import Start from './components/Start';
+import Chat from './components/Chat';
 
-  alertMyText (input = []) {
-    Alert.alert(input.text);
-  }
+//import react native gesture handler
+import 'react-native-gesture-handler';
+
+//import react Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+export default class App extends React.Component {
 
   render() {
     return (
-      <View style={{flex: 1, justifyContent: 'center'}}>
-        <TextInput 
-          style={{height: 40, borderColor: 'gray', borderWidth: 2}}
-          onChangeText={(text) => 
-          this.setState({text})}
-          value={this.state.text}
-          placeholder='Type here...'
-        />
-        <Text>You wrote: {this.state.text}</Text>
-        <Button onPress={() => {
-          this.alertMyText({text: this.state.text});
-        }}
-        title='Press Me'
-        />
-        <ScrollView>
-          <Text style={{fontSize: 110}}>This text is so bog! And so long! You have to scroll!</Text>
-        </ScrollView>
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName='Start'
+          >
+            <Stack.Screen
+            name='Start'
+            component={Start}
+            />
+            <Stack.Screen
+            name='Chat'
+            component={Chat}
+            />
+          </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column'
-  },
-  box1: {
-    flex: 10,
-    backgroundColor: 'red'
-  },
-  box2: {
-    flex: 120,
-    backgroundColor: 'blue'
-  },
-  box3: {
-    flex: 50,
-    backgroundColor: 'green'
-  }
-})
